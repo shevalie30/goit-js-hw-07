053cef9969328871fd0f92aacecdac87ec1059f3
shevalie30 <103785982+shevalie30@users.noreply.github.com>
2022-08-29 18:42:31 +0300

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const ref = {
+    containerForGallery: document.querySelector('.gallery'),
+}
+
+let galleryItem = '';
+const buildGallery = galleryItems.map((key) => {
+    galleryItem += `
+        <div class="gallery__item">
+        <a class="gallery__link" href="#">
+                <img
+                    class="gallery__image"
+                    src="${key.preview}"
+                    data-source="${key.original}"
+                    alt="${key.description}"
+                />
+                </a>
+        </div>`;
+})
+
+ref.containerForGallery.insertAdjacentHTML("afterbegin", galleryItem)
+
+ref.containerForGallery.addEventListener(`click`, openModalWindow)
+
+function openModalWindow(event) {
+    console.log(event.target.dataset.source);
+    event.preventDefault();
+    const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    </div>
+	`);
+    instance.show();
+    window.addEventListener('keydown', function (e) {
+
+        if (!basicLightbox.visible()) { return };
+        if (e.key == 'Escape' || e.key == 'Esc' || e.key == 27) {
+            e.preventDefault();
+            instance.close();
+        }
+    });
 
 }
+
+console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+
+const ref = {
+    containerForGallery: document.querySelector('.gallery'),
+};
+
+let galleryItem = '';
+const buildGallery = galleryItems.map(({ original, preview, description }) => {
+    galleryItem += `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+                </a>
+        </div>`;
+});
+
+ref.containerForGallery.innerHTML = galleryItem;
+
+
+new SimpleLightbox('.gallery__link', {
+    captionsData: "alt",
+    captionDelay: 250,
+
+});
 console.log(galleryItems);
